696e1f8c768e1983d9725a82a47727f402c7b5fb
nielsklerk <90217677+nielsklerk@users.noreply.github.com>
2021-09-10 18:27:21 +0200

    1 more

--- PWS_code.py
+++ PWS_code.py
@@ -1,2 +1,26 @@
-print("hello world")
-hello
+import pygame
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 1000
+screen_height = 1000
+tile_size = 50
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("PWS")
+
+run = True
+while run:
+
+    pygame.display.update()
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+pygame.quit()
