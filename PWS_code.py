8f0f6312e0251c1ead404b4bb6d9855a04eada38
nielsklerk <90217677+nielsklerk@users.noreply.github.com>
2021-09-20 21:32:08 +0200

    1 more

--- PWS_code.py
+++ PWS_code.py
@@ -5,78 +5,80 @@ pygame.init()
 clock = pygame.time.Clock()
 fps = 60
 
-screen_width = 1000
-screen_height = 1000
-tile_size = 50
+screen_width = 1024
+screen_height = 576
+tile_size = 64
+game_over = False
+
+moving_left = False
+moving_right = False
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("PWS")
 
 
-class PLayer:
-    def __init__(self, x, y):
-        self.images_right = []
-        self.images_left = []
-        self.index = 0
-        for num in range(1, 4):
-            img_right = pygame.image.load(f"Player{num}.png")
-            img_left = pygame.transform.flip(img_right, True, False)
-            self.images_right.append(img_right)
-            self.images_right.append(img_left)
-        self.image = self.images_right[self.index]
+def draw_bg():
+    screen.fill((100, 100, 100))
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = speed
+        self.direction = 1
+        self.flip = False
+        img = pygame.image.load("Player0.png")
+        self.image = pygame.transform.scale(img, (100, 50))
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
+        self.rect.center = (x, y)
 
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
 
-class World:
-    def __init__(self, data):
-        self.tile_list = []
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
 
-        row_count = 0
-        for row in data:
-            col_count = 0
-            for tile in row:
-                if tile == 1:
-                    img = pygame.transform.scale(_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                if tile == 2:
-                    img = pygame.transform.scale(_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                if tile == 3:
-                    img = pygame.transform.scale(_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                col_count += 1
-            row_count += 1
+        self.rect.x += dx
+        self.rect.y += dy
 
     def draw(self):
-        for tile in self.tile_list:
-            screen.blit(tile[0], tile[1])
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
+
+player = Player(100, 100, 5)
 
 run = True
 while run:
 
-    pygame.display.update()
+    player.draw()
+
+    player.move(moving_left, moving_right)
     clock.tick(fps)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+
+    pygame.display.update()
 
 pygame.quit()
