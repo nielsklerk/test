ef8db8ddc97b32b3527a8582f2bf0eaac689ecc0
thuyd <tdam101296@gmail.com>
2021-12-24 00:08:58 +0100

    - setup.py file fixed

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-import cx_Freeze
+import sys
+import os
+from cx_Freeze import setup, Executable
 
-executables = [cx_Freeze.Executable("PWS_code.py")]
+#add files
+files = ['img/', 'audio/']
 
-cx_Freeze.setup(
-    name="Re:birth",
-    options={"build_exe": {"packages"}: [pygame],
-"include_files": [img]}),
-executables = executables
+#target
 
+target = Executable(
+    script="PWS_code.py",
+    base="Win32GUI",
+    icon="img"
+)
+#SETUP CX_FREEZE
+setup(
+    name="Pycharm",
+    version="3.0",
+    description="Modern GUI for python applications",
+    author="JetBrains",
+    options={'build_exe': {'include_files': files}},
+    executables=[target]
 )
